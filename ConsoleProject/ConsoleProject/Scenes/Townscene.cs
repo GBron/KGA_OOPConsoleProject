5066f4031ebec873f6077d0ec77234762b527f5b
GBron <zmflatod@naver.com>
2025-04-08 23:23:58 +0900

    [Chore] 마을 선택지 수정
    마을 선택지 바위의 성검 제거
    아이템 구현 후 추가 예정

--- ConsoleProject/ConsoleProject/Scenes/Townscene.cs
+++ ConsoleProject/ConsoleProject/Scenes/Townscene.cs
@@ -13,8 +13,8 @@
             Util.PrintLine("1. 상점");
             Util.PrintLine("2. 여관");
             Util.PrintLine("3. 게시판 확인");
-            Util.PrintLine("4. 검 박힌 바위");
-            Util.PrintLine("5. 마을 밖으로");
+            // Util.PrintLine("4. 검 박힌 바위"); 아이템 구현 시 넣기
+            Util.PrintLine("4. 마을 밖으로");
         }
         public override void Reaction()
         {
@@ -29,15 +29,15 @@
                 case ConsoleKey.D3:
                     Util.PrintLine("게시판을 확인합니다.");
                     break;
+                // case ConsoleKey.D4:
+                //     Util.PrintLine("마을의 명물, 성검이 박힌 바위로 다가갑니다.");
+                //     break;
                 case ConsoleKey.D4:
-                    Util.PrintLine("마을의 명물, 성검이 박힌 바위로 다가갑니다.");
-                    break;
-                case ConsoleKey.D5:
                     Util.PrintLine("마을 밖으로 나갑니다.");
                     break;
             }
         }
-
+        w
         public override void Wait()
         {
             Util.PrintLine("아무키나 눌러주세요.", ConsoleColor.DarkGray);
@@ -57,10 +57,10 @@
                 case ConsoleKey.D3:
                     GameManager.ChangeScene("BulletinBoard");
                     break;
+                // case ConsoleKey.D4:
+                // // 성검이 박힌 바위 씬으로 이동
+                // break;
                 case ConsoleKey.D4:
-                    // 성검이 박힌 바위 씬으로 이동
-                    break;
-                case ConsoleKey.D5:
                     GameManager.ChangeScene("Field");
                     break;
             }
